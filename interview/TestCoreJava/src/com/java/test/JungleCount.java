185b164e13a8af375c2fc663531d24e9cee94b3e
Mukesh <mkmishra.se@gmail.com>
2024-09-17 18:22:42 +0530

    First all practice code commit.

---
+++ interview/TestCoreJava/src/com/java/test/JungleCount.java
@@ -0,0 +1,24 @@
+package com.java.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JungleCount {
+	    public static void main(String[] args) {
+	        String input = "jungleWelcometoJuNgLe";
+	        String wordToFind = "jungle";
+	        int count = countOccurrences(input, wordToFind);
+	        System.out.println("Number of times '" + wordToFind + "' appears: " + count);
+	    }
+	    
+	    public static int countOccurrences(String input, String wordToFind) {
+	        String regex = "(?i)" + wordToFind;
+	        int count = 0;
+	        Pattern pattern = Pattern.compile(regex);
+	        Matcher matcher = pattern.matcher(input);
+	        while (matcher.find()) {
+	            count++;
+	        }
+	        return count;
+	    }
+	}
